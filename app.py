dfeaa79b6670dd33d37d45ccdfa51ae0f33e0cde
vijeshkumar4545 <vijeshkumar24242424@gmail.com>
2026-02-23 17:12:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -36,4 +36,5 @@ def index():
     return render_template("index.html")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
